849758fa97893ab16c7ae2619fcb2730c613eb94
Bishal Bhansali <jainbishal@me.com>
2020-10-26 18:13:40 +1100

    New updates

---
+++ FitnessPal/AddWorkout.swift
@@ -0,0 +1,23 @@
+//
+//  AddWorkout.swift
+//  FitnessPal
+//
+//  Created by Bishal Bhansali on 17/10/20.
+//  Copyright © 2020 Tutorial. All rights reserved.
+//
+
+import UIKit
+
+class AddWorkout: UIViewController {
+    override func viewDidLoad() {
+          super.viewDidLoad()
+            title = "Add Workout"
+          // Do any additional setup after loading the view.
+      }
+    
+    
+    
+    
+    
+    
+}
---
+++ FitnessPal/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  FitnessPal
+//
+//  Created by Bishal Bhansali on 17/10/20.
+//  Copyright © 2020 Tutorial. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var lblMessage: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        lblMessage.text = "Himmat"
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+}
+
