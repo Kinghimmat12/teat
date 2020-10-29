c142421168d4dfbffebea6beac256a40f4eaf83c
kinghimmat <Kinghimmat12@gmail.com>
2020-10-29 19:10:50 +1100

    i added stuff you asked me two thank you also for the scroll thing i have been trying for soo long.

--- FitnessPal/AddWorkout.swift
+++ FitnessPal/AddWorkout.swift
@@ -7,15 +7,32 @@
 //
 
 import UIKit
-
+import ActionSheetPicker_3_0
 class AddWorkout: UIViewController {
     override func viewDidLoad() {
           super.viewDidLoad()
-            title = "Add Workout"
+            title = "Add Workou"
           // Do any additional setup after loading the view.
       }
     
     
+    @IBAction func actshowpicker(_ sender: Any) {
+        
+        
+        
+       ActionSheetMultipleStringPicker.show(withTitle: "Multiple String Picker", rows: [
+           ["One", "Two", "A lot"],
+           ["Many", "Many more", "Infinite"]
+           ], initialSelection: [2, 2], doneBlock: {
+               picker, indexes, values in
+
+               print("values = \(values)")
+               print("indexes = \(indexes)")
+               print("picker = \(picker)")
+               return
+       }, cancel: { ActionMultipleStringCancelBlock in return }, origin: sender)
+        
+    }
     
     
     
